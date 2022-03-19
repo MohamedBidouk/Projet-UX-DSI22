fe706ee9b7bd31e39290cb331d357db6d510de2c
MohamedBidouk <mohamedbidouk1998@gmail.com>
2022-03-19 15:21:42 +0100

    preFinish

---
+++ js/main.js
@@ -0,0 +1,91 @@
+var speed = 20;
+
+/* Call this function with a string containing the ID name to
+ * the element containing the number you want to do a count animation on.*/
+function incEltNbr(id) {
+  elt = document.getElementById(id);
+  endNbr = Number(document.getElementById(id).innerHTML);
+  incNbrRec(0, endNbr, elt);
+}
+
+/*A recursive function to increase the number.*/
+function incNbrRec(i, endNbr, elt) {
+  if (i <= endNbr) {
+    elt.innerHTML = i;
+    setTimeout(function() {//Delay a bit before calling the function again.
+      incNbrRec(i + 1, endNbr, elt);
+    }, speed);
+  }
+}
+
+function incEltNbr2(id) {
+    elt = document.getElementById(id);
+    endNbr = Number(document.getElementById(id).innerHTML);
+    incNbrRec2(0, endNbr, elt);
+  }
+  
+  /*A recursive function to increase the number.*/
+  function incNbrRec2(i, endNbr, elt) {
+    if (i <= endNbr) {
+      elt.innerHTML = i;
+      setTimeout(function() {//Delay a bit before calling the function again.
+        incNbrRec2(i + 1, endNbr, elt);
+      }, 10);
+    }
+  }
+
+/*Function called on button click*/
+ /*Call this funtion with the ID-name for that element to increase the number within*/
+
+
+  
+   /*Call this funtion with the ID-name for that element to increase the number within*/
+
+  $(document).scroll(function() {
+    var y = $(this).scrollTop();
+    if (y = 4) {
+        function incNbr(){
+            incEltNbr("nbr");
+          }
+          
+          incEltNbr("nbr");
+          function incNbr(){
+            incEltNbr("nbr1");
+          }
+          
+          incEltNbr("nbr1"); /*Call this funtion with the ID-name for that element to increase the number within*/
+          
+          function incNbr2(){
+            incEltNbr2("nbr2");
+          }
+          incEltNbr2("nbr2"); /*Call this funtion with the ID-name for that element to increase the number within*/
+
+        function incNbr(){
+            incEltNbr("nbr3");
+        }
+        
+        incEltNbr("nbr3");
+    } else {
+        function incNbr(){
+            incEltNbr("nbr");
+          }
+          
+          incEltNbr("nbr");
+    }
+    function incNbr(){
+        incEltNbr("nbr1");
+      }
+      
+      incEltNbr("nbr1"); /*Call this funtion with the ID-name for that element to increase the number within*/
+      
+      function incNbr2(){
+        incEltNbr2("nbr2");
+      }
+      incEltNbr2("nbr2"); /*Call this funtion with the ID-name for that element to increase the number within*/
+
+    function incNbr(){
+        incEltNbr("nbr3");
+    }
+    
+    incEltNbr("nbr3");
+  });
