7227151642b8a5e50dfb48f417c7f13b9db72ab6
MohamedBidouk <mohamedbidouk1998@gmail.com>
2022-03-21 08:19:21 +0100

    presque sayé

--- js/main.js
+++ js/main.js
@@ -89,3 +89,33 @@ function incEltNbr2(id) {
     
     incEltNbr("nbr3");
   });
+
+
+
+
+
+
+
+
+
+  document.addEventListener("DOMContentLoaded", () => {
+    function counter(id, start, end, duration) {
+     let obj = document.getElementById(id),
+      current = start,
+      range = end - start,
+      increment = end > start ? 1 : -1,
+      step = Math.abs(Math.floor(duration / range)),
+      timer = setInterval(() => {
+       current += increment;
+       obj.textContent = current;
+       if (current == end) {
+        clearInterval(timer);
+       }
+      }, step);
+    }
+    counter("count1", 0, 5890, 3000);
+    counter("count2", 0, 478, 3000);
+    counter("count3", 0, 78, 2800);
+    counter("count", 0, 26, 1500)
+   });
+   
